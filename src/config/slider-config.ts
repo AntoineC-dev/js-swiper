7c8ddd7b8e89ec1f4fb90a8fd6f5e6b1c5537a23
Antoine Cheminat <ac.dev@hotmail.com>
2022-07-26 17:45:13 -0400

    Create slider config

---
+++ src/config/slider-config.ts
@@ -0,0 +1,38 @@
+// Elements
+export const slider = document.querySelector("[data-slider]") as Element;
+export const innerSlider = document.querySelector("[data-inner-slider]") as Element;
+export const sliderPagination = document.querySelector("[data-slider-pagination]") as Element;
+
+// NodeListOf<Element>
+export const slides = document.querySelectorAll("[data-slide]");
+export const sliderBtns = document.querySelectorAll("[data-slider-btn]");
+
+// Const
+export const SLIDER_SPACING = 16;
+export const PAGINATION_ACTIVE_CLASS = "pagination-active";
+export const PAGINATION_ACTIVE_COLOR = "bg-zinc-700";
+export const PAGINATION_DEFAULT_COLOR = "bg-zinc-200";
+export const SWIPER_THRESHOLD = 0.25;
+
+// Config
+export type SliderConfigType = {
+  currentIndex: number;
+  isMoving: boolean;
+  lastIndex: number;
+  nbOfSlides: number;
+  nbPerSlide: number;
+  slideWidth: number;
+  offsetLeft: number;
+  maxOffsetLeft: number;
+};
+
+export let sliderConfig: SliderConfigType = {
+  currentIndex: 0,
+  isMoving: false,
+  lastIndex: 0,
+  nbOfSlides: 0,
+  nbPerSlide: 0,
+  slideWidth: slides[0].clientWidth,
+  offsetLeft: 0,
+  maxOffsetLeft: 0,
+};
